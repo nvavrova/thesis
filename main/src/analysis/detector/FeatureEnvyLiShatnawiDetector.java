efef3db990ed915c40d4d268ac132818b73fb517
nvavrova <vavrova.n@gmail.com>
2015-11-11 00:05:25 +0100

    fixed feature envy to pertain to method, not class

--- main/src/analysis/detector/FeatureEnvyLiShatnawiDetector.java
+++ main/src/analysis/detector/FeatureEnvyLiShatnawiDetector.java
@@ -1,14 +1,11 @@
 package analysis.detector;
 
 import analysis.Metric;
-import model.Class;
 import model.Subroutine;
+import model.SubroutineType;
 
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * Created by Nik on 09-11-2015
@@ -16,47 +13,33 @@ import java.util.stream.Collectors;
 public class FeatureEnvyLiShatnawiDetector extends Detector {
 	//AID > 4 and AID in top 10% and ALD < 3 and referenced classes < 3
 
-	private final Map<String, Set<Integer>> functionAids;
+	private final Map<String, Integer> subroutineAids;
 
 	public FeatureEnvyLiShatnawiDetector() {
-		this.functionAids = new HashMap<>();
+		this.subroutineAids = new HashMap<>();
 	}
 
 	@Override
-	protected Boolean isPreliminarilyDefective(Class cls) {
-		Set<Integer> functionAids = new HashSet<>();
+	protected Boolean isPreliminarilyDefective(Subroutine subroutine) {
+		SubroutineType subType = subroutine.getSubroutineType();
+		boolean check = (subType == SubroutineType.INSTANCE_METHOD || subType == SubroutineType.STATIC_METHOD)
+				&& subroutine.getAccessOfImportData() > 4 && subroutine.getAccessOfLocalData() < 3
+				&& subroutine.getReferencedClassesSet().size() < 3;
 
-		for (Subroutine subroutine : cls.getDefinedSubroutinesSet()) {
-			if (subroutine.getAccessOfImportData() > 4 && subroutine.getAccessOfLocalData() < 3
-					&& this.getReferencedNonSuperclassClasses(cls.getParentsSet(), subroutine).size() < 3) {
-				functionAids.add(subroutine.getAccessOfImportData());
-			}
-		}
-		boolean check = functionAids.size() > 0;
 		if (check) {
-			this.functionAids.put(cls.getFullPath(), functionAids);
+			this.subroutineAids.put(subroutine.getFullPath(), subroutine.getAccessOfImportData());
 		}
+
 		return check;
 	}
 
 	@Override
 	protected Boolean confirmDefect(String fullPath) {
-		for (Integer aid : this.functionAids.get(fullPath)) {
-			if (this.metrics.isInTop(Metric.SUBROUTINE_AID, 10, aid)) {
-				return true;
-			}
-		}
-		return false;
+		return this.metrics.isInTop(Metric.SUBROUTINE_AID, 10, this.subroutineAids.get(fullPath));
 	}
 
 	@Override
 	protected String getName() {
 		return "Feature Envy (Li & Shatnawi)";
-	}
-
-	protected Set<Class> getReferencedNonSuperclassClasses(Set<Class> superclasses, Subroutine subroutine) {
-		return subroutine.getReferencedClassesSet().stream()
-				.filter(c -> !superclasses.contains(c))
-				.collect(Collectors.toSet());
 	}
 }
